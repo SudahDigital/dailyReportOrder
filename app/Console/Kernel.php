f69d6f2dc175de8d26927a1bb4828d2aa28aa308
Zuki Setiawan <setyawanzuky@gmail.com>
2022-03-28 22:08:48 +0700

    Update scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call('App\Http\Controllers\SendReportDailyController@index')->daily()->at('22:00:01');
+        $schedule->call('App\Http\Controllers\SendReportDailyController@index')->daily()->at('22:15:59');
         //$schedule->command('App\Http\Controllers\SendReportDailyController@index')->everyMinute();
     }
 
