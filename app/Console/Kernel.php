07ecf2ff1a86bf21e32b6c7d963d1c0eb1b043af
Zuki Setiawan <setyawanzuky@gmail.com>
2022-03-28 16:11:00 +0700

    Update Dockerfile

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call('App\Http\Controllers\SendReportDailyController@index')->daily()->at('16:05');
+        $schedule->call('App\Http\Controllers\SendReportDailyController@index')->daily()->at('16:20');
         //$schedule->command('App\Http\Controllers\SendReportDailyController@index')->everyMinute();
     }
 
